d6b5118bed756aedaa660e85ef3b8fd42daaa4f5
Jared Cosulich <jared.cosulich@gmail.com>
2019-02-26 10:41:48 -0500

    upload file to s3 before redirecting to it

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const chromium = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
 const mhtml2html = require('mhtml2html');
+const aws = require('aws-sdk');
+const s3 = new aws.S3();
 
+const bucket = "atlas-page-saver"
 const services = [
   'google_doc'
 ]
@@ -9,7 +12,9 @@ const services = [
 exports.handler = async (event, context) => {
   let result = null;
   let browser = null;
-  const url = (event.queryStringParameters || event).url || 'http://www.example.com';
+  const params = event.queryStringParameters || event;
+  const url = params.url || 'http://www.example.com';
+  const username = params.username || "";
 
   try {
     browser = await puppeteer.launch({
@@ -36,12 +41,33 @@ exports.handler = async (event, context) => {
 
     const mhtmlParsed = mhtml2html.parse(data);
     const htmlDocument = mhtml2html.convert(mhtmlParsed);
+
+    var filename = htmlDocument.title.replace(/\s/g, "-") + ".html";
+    if (username) {
+      filename = username + "/" + filename;
+    }
+
+    const request = await s3.putObject({
+      Bucket: bucket,
+      Key: filename,
+      Body: htmlDocument.documentElement.outerHTML,
+      ContentType: "text/html"
+    });
+
+    var statusCode = 302;
+    var body;
+    await request.on('error', function(response) {
+      statusCode = 500;
+      body = response.error;
+    }).
+    send();
+
     result = {
-      "statusCode": 200,
+      "statusCode": statusCode,
       "headers": {
-        "title": htmlDocument.title
+        "Location": "https://s3.amazonaws.com/" + bucket + "/" + filename
       },
-      "body": htmlDocument.documentElement.outerHTML
+      "body": body
     };
 
   } catch (error) {
